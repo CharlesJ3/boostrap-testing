6dbb58f01b41c2a4ecbba5348eb9c20be074c6e6
Charles Jones <charlesj@jsectechnologies.com>
2018-07-18 15:47:06 -0600

    Adding some scripting for local testing

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+let todos = ['item 1', 'item 2', 'item 3', 'item 4', 'item 5'];
+
+console.log(todos);
+
+//delete 3rd item
+todos.splice(2, 1);
+
+console.log(todos);
+
+//add new item to the end
+todos.push('Item 6');
+
+console.log(todos);
+
+//rmeove first item from list
+todos.shift();
+
+console.log(todos);
